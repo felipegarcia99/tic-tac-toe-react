8b2aff123f601a7aa0b4bc3ef1a4678a3b5a73a7
Felipe Garcia <78655428+felipegarcia99@users.noreply.github.com>
2021-07-15 20:40:27 -0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,20 @@
+import Board from './board';
+import React from 'react';
+
+class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+}
+
+export default Game;
